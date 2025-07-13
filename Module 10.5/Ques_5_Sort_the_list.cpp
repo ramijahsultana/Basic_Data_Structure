785680f6a2a15f2ae7e535cdd3df8f2f6368322b
Rafa <ramijahsultana@gmail.com>
2025-07-13 21:43:41 +0600

    Practice

---
+++ Module 10.5/Ques_5_Sort_the_list.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+void Insert_at_head(Node *&head, Node *&tail, int val)
+{
+    Node *new_node = new Node(val);
+    if (head == NULL)
+    {
+        head = new_node;
+        tail = new_node;
+        return;
+    }
+    new_node->next = head;
+    head->prev = new_node;
+    head = new_node;
+}
+
+void Insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *new_node = new Node(val);
+    if (head == NULL)
+    {
+        head = new_node;
+        tail = new_node;
+        return;
+    }
+    tail->next = new_node;
+    new_node->prev = tail;
+    tail = new_node;
+}
+
+void Print_list_forward(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+void Print_list_backward(Node *tail)
+{
+    Node *temp = tail;
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->prev;
+    }
+    cout << endl;
+}
+
+void sort_the_list(Node* head, Node* tail, int val)
+{
+    for(Node* i=head, *j=tail; i!=j && i->prev)
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int val;
+    
+    while (true)
+    {
+        cin >> val;
+        if(val == -1) break;
+        Insert_at_tail(head, tail, val);
+    }
+
+   
+
+    return 0;
+}
